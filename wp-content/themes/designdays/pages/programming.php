7db3265ded5e01429cb37f3b67ee8d9e83e9b246
dasaaf <as.f09942@gmail.com>
2022-04-08 11:22:57 -0500

    Carrusel listo!!!

--- wp-content/themes/designdays/pages/programming.php
+++ wp-content/themes/designdays/pages/programming.php
@@ -1,91 +1,37 @@
 <div class="programming-container">
     <h1 class="programming-title">PROGRAMACIÓN</h1>
-    <div class="pure-g events-container">
+    <div class="arrow-container">
+        <div class="left-arrow"></div>
+        <div class="right-arrow"></div>
+    </div>
+    <div class="events-container">
 
-        <div class="arrow-container">
-            <div class="left-arrow"></div>
-            <div class="right-arrow"></div>
-        </div>
+        <div class="right-arrow-background"></div>
+        <div class="all-events-container">
+            <?php
+            for ($i = 0; $i < 10; $i++) {
+            ?>
 
-        <div class="pure-u-1 pure-u-lg-6-24">
-            <div class="event-card-container">
-                <div class="event-card-date">
-                    <div class="event-date-month">SEPTIEMBRE</div>
-                    <div class="event-date-day">25</div>
-                    <div class="event-date-day-name">MIÉRCOLES</div>
-                </div>
-                <div class="event-description-container">
-                    <div class="author">PH. D. FELIPE CESAR LONDOÑO LÓPEZ</div>
-                    <div class="description">
-                        "Diseño en postpandemia" ¿Qué hacen hoy las ciudades para retomar a la normalidad y qué papel cumple el diseño en este proceso?
+                <div class="event-card-container">
+                    <div class="event-card-date">
+                        <div class="event-date-month">SEPTIEMBRE</div>
+                        <div class="event-date-day">25</div>
+                        <div class="event-date-day-name">MIÉRCOLES</div>
+                    </div>
+                    <div class="event-description-container">
+                        <div class="author">PH. D. FELIPE CESAR LONDOÑO LÓPEZ</div>
+                        <div class="description">
+                            "Diseño en postpandemia" ¿Qué hacen hoy las ciudades para retomar a la normalidad y qué papel cumple el diseño en este proceso?
+                        </div>
+                    </div>
+                    <div class="event-hour">09:00 AM</div>
+                    <div class="event-background">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/background-events.png" alt="">
                     </div>
                 </div>
-                <div class="event-hour">09:00 AM</div>
-                <div class="event-background">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/background-events.png" alt="">
-                </div>
-            </div>
+            <?php
+            }
+            ?>
         </div>
-
-        <div class="pure-u-1 pure-u-lg-6-24">
-            <div class="event-card-container">
-                <div class="event-card-date">
-                    <div class="event-date-month">SEPTIEMBRE</div>
-                    <div class="event-date-day">25</div>
-                    <div class="event-date-day-name">MIÉRCOLES</div>
-                </div>
-                <div class="event-description-container">
-                    <div class="author">PH. D. FELIPE CESAR LONDOÑO LÓPEZ</div>
-                    <div class="description">
-                        "Diseño en postpandemia" ¿Qué hacen hoy las ciudades para retomar a la normalidad y qué papel cumple el diseño en este proceso?
-                    </div>
-                </div>
-                <div class="event-hour">09:00 AM</div>
-                <div class="event-background">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/background-events.png" alt="">
-                </div>
-            </div>
-        </div>
-
-        <div class="pure-u-1 pure-u-lg-6-24">
-            <div class="event-card-container">
-                <div class="event-card-date">
-                    <div class="event-date-month">SEPTIEMBRE</div>
-                    <div class="event-date-day">25</div>
-                    <div class="event-date-day-name">MIÉRCOLES</div>
-                </div>
-                <div class="event-description-container">
-                    <div class="author">PH. D. FELIPE CESAR LONDOÑO LÓPEZ</div>
-                    <div class="description">
-                        "Diseño en postpandemia" ¿Qué hacen hoy las ciudades para retomar a la normalidad y qué papel cumple el diseño en este proceso?
-                    </div>
-                </div>
-                <div class="event-hour">09:00 AM</div>
-                <div class="event-background">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/background-events.png" alt="">
-                </div>
-            </div>
-        </div>
-
-        <div class="pure-u-1 pure-u-lg-6-24">
-            <div class="event-card-container">
-                <div class="event-card-date">
-                    <div class="event-date-month">SEPTIEMBRE</div>
-                    <div class="event-date-day">25</div>
-                    <div class="event-date-day-name">MIÉRCOLES</div>
-                </div>
-                <div class="event-description-container">
-                    <div class="author">PH. D. FELIPE CESAR LONDOÑO LÓPEZ</div>
-                    <div class="description">
-                        "Diseño en postpandemia" ¿Qué hacen hoy las ciudades para retomar a la normalidad y qué papel cumple el diseño en este proceso?
-                    </div>
-                </div>
-                <div class="event-hour">09:00 AM</div>
-                <div class="event-background">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/background-events.png" alt="">
-                </div>
-            </div>
-        </div>
-
     </div>
 </div>
