222794fb5fb08e64cfa21e1975dc2c9f9d3922be
dasaaf <as.f09942@gmail.com>
2022-05-10 10:52:54 -0500

    Improvements in footer responsive

--- wp-content/themes/designdays/pages/footer.php
+++ wp-content/themes/designdays/pages/footer.php
@@ -1,12 +1,12 @@
 <div class="footer-container">
     <div class="pure-g">
-        <div class="pure-u-1 pure-u-lg-8-24">
+        <div class="pure-u-1 pure-u-lg-8-24 pure-u-sm-24-24">
             <?php include 'contact-form.php' ?>
         </div>
-        <div class="interest-links pure-u-1 pure-u-lg-8-24">
+        <div class="interest-links pure-u-1 pure-u-lg-8-24 pure-u-sm-24-24">
             <?php include 'links.php' ?>
         </div>
-        <div class="footer-logo pure-u-1 pure-u-lg-8-24">
+        <div class="footer-logo pure-u-1 pure-u-lg-8-24 pure-u-sm-24-24">
             <div class="logotipo">
                 <div class="logotipo-dias">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-amarillo.png" alt="logotipo-consultorio">
