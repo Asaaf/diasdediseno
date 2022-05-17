d1ee0616635855fa6bb0dfe02bead288c6cc9c81
dasaaf <as.f09942@gmail.com>
2022-05-17 10:41:10 -0500

    It's added the flamingo plugin for store mails of contact

--- wp-content/themes/designdays/pages/contact-form.php
+++ wp-content/themes/designdays/pages/contact-form.php
@@ -1,5 +1,9 @@
 <div class="contact-form-container">
     <h1 class="contact-title">ESCRÍBENOS</h1>
+    <?php
+    echo do_shortcode("[contact-form-7 id='34' title='Formulario de contacto footer']");
+    ?>
+    <!--
     <form action="#" method="post" class="form-container">
         <div class="input-form">
             <input type="email" name="email" id="email" class="input-field" placeholder="Correo">
@@ -16,4 +20,24 @@
             </div>
         </div>
     </form>
+-->
+
+    <!--
+
+CONTACT FORM
+
+<div class="input-form">
+    [email* your-email class:input-field id:email watermark "Correo"]
+</div>
+<div class="input-form">
+    [text* your-subject class:input-field id:subject watermark "Asunto"]
+</div>
+
+<div class="input-form">
+    [textarea your-message class:input-field id:body watermark "Asunto"]
+</div>
+<div class="btn-send">
+[submit "Enviar" class:send]
+</div>
+-->
 </div>
